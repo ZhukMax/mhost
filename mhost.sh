3e3b036832d94ab6bd7427f1e3562aeeb31226cd
Max Zhuk <zhukmax@ya.ru>
2016-09-07 21:11:55 +0300

    Update mhost.sh

--- mhost.sh
+++ mhost.sh
@@ -1,1 +1,54 @@
 #!/bin/bash
+
+echo "Enter username for site and database:"
+read USERNAME
+ 
+echo "Enter domain"
+read DOMAIN
+
+echo "Creating vhost file"
+echo "
+server {
+	listen				80;
+	server_name			$DOMAIN www.$DOMAIN;
+	root				/var/www/$USERNAME/public;
+	access_log			/var/www/$USERNAME/logs/access.log;
+	error_log			/var/www/$USERNAME/logs/error.log;
+	index				index.php index.html;
+	rewrite_log			on;
+	
+	if (\$host != '$DOMAIN' ) {
+		rewrite			^/(.*)$  http://$DOMAIN/\$1  permanent;
+	}
+	location ~* ^/core/ {
+		deny			all;
+	}
+	location / {
+		try_files		\$uri \$uri/ @rewrite;
+	}
+	location /index.html {
+		rewrite			/ / permanent;
+	}
+ 
+	location ~ ^/(.*?)/index\.html$ {
+		rewrite			^/(.*?)/ /$1/ permanent;
+	}
+	location @rewrite {
+		rewrite			^/(.*)$ /index.php?q=\$1;
+	}
+	location ~ \.php$ {
+		include			fastcgi_params;
+		fastcgi_param	SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+		fastcgi_pass	unix:/var/run/php7.0-fpm.sock;
+	}
+	location ~* ^.+\.(jpg|jpeg|gif|css|png|js|ico|bmp)$ {
+	   access_log		off;
+	   expires			10d;
+	   break;
+	}
+	location ~ /\.ht {
+		deny			all;
+	}
+}
+" > /etc/nginx/sites-available/$USERNAME.conf
+ln -s /etc/nginx/sites-available/$USERNAME.conf /etc/nginx/sites-enabled/$USERNAME.conf
