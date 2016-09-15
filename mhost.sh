5db7decae36a11f12a6ec2c1765a265c366bf82f
Max Zhuk <zhukmax@ya.ru>
2016-09-15 15:18:11 +0300

    Update mhost.sh

--- mhost.sh
+++ mhost.sh
@@ -6,6 +6,13 @@ read USERNAME
 echo "Enter domain"
 read DOMAIN
 
+mkdir /var/www/$USERNAME
+mkdir /var/www/$USERNAME/tmp
+mkdir /var/www/$USERNAME/logs
+mkdir /var/www/$USERNAME/public
+chmod -R 755 /var/www/$USERNAME/
+chown www-data:www-data /var/www/$USERNAME
+
 echo "Creating vhost file"
 echo "
 server {
@@ -52,3 +59,22 @@ server {
 }
 " > /etc/nginx/sites-available/$USERNAME.conf
 ln -s /etc/nginx/sites-available/$USERNAME.conf /etc/nginx/sites-enabled/$USERNAME.conf
+
+service nginx restart
+service php7.0-fpm restart
+
+echo "Enter MySQL root password:"
+read ROOTPASS
+
+echo "Enter password for new DB:"
+read MYSQLPASS
+
+echo "Creating database"
+
+Q1="CREATE DATABASE IF NOT EXISTS $USERNAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;;"
+Q2="GRANT ALTER,DELETE,DROP,CREATE,INDEX,INSERT,SELECT,UPDATE,CREATE TEMPORARY TABLES,LOCK TABLES ON $USERNAME.* TO '$USERNAME'@'localhost' IDENTIFIED BY '$MYSQLPASS';"
+Q3="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}"
+	
+mysql -uroot --password=$ROOTPASS -e "$SQL"
+
