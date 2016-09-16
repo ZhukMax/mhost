de015acedcf360d08e75d50b5482e10139c241f3
Max Zhuk <zhukmax@ya.ru>
2016-09-16 19:54:12 +0300

    Update mhost.sh

--- mhost.sh
+++ mhost.sh
@@ -5,17 +5,34 @@ read USERNAME
  
 echo "Enter domain"
 read DOMAIN
+ 
+echo "Blank (b), new (n) or exists (x) project?"
+echo "default - blank (b):"
+read PROJECT
 
 mkdir /var/www/$USERNAME
 mkdir /var/www/$USERNAME/tmp
 mkdir /var/www/$USERNAME/logs
-cd /var/www/$USERNAME
-wget https://github.com/modxcms/revolution/archive/2.x.zip
-unzip 2.x.zip
-mv -R /var/www/$USERNAME/revolution-2.x /var/www/$USERNAME/public
-cd public
-rm -rf _build
-rm -f .gitignore .travis.yml .editorconfig README.md PULL_REQUEST_TEMPLATE.md ht.access CONTRIBUTING.md
+
+if [ $PROJECT == n ] || [$PROJECT == new]
+then
+	cd /var/www/$USERNAME
+	wget https://github.com/modxcms/revolution/archive/2.x.zip
+	unzip 2.x.zip
+	mv -R /var/www/$USERNAME/revolution-2.x /var/www/$USERNAME/public
+	cd public
+	rm -rf _build
+	rm -f .gitignore .travis.yml .editorconfig README.md PULL_REQUEST_TEMPLATE.md ht.access CONTRIBUTING.md
+	cd ~
+elif [ $PROJECT == x ] || [$PROJECT == exists]
+then
+	cd /var/www/$USERNAME
+	echo "Enter url to your git-repository:"
+	read REPO
+	git clone $REPO
+	cd ~
+fi
+
 chmod -R 755 /var/www/$USERNAME/
 chown www-data:www-data /var/www/$USERNAME
 
